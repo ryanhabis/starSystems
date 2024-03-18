b8b1275860773d32fd2c1b662c1ae82441b19182
Ryan Habis <ryan.habiss@gmail.com>
2024-03-18 15:44:31 +0000

    Adding starSystems.Models

--- starSystems/Controllers/StarSystemController.cs
+++ starSystems/Controllers/StarSystemController.cs
@@ -4,9 +4,22 @@ namespace starSystems.Controllers
 {
     public class StarSystemController : Controller
     {
-        public IActionResult Index()
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        // 
+        // GET: /HelloWorld/
+        public string Index()
         {
-            return View();
+            return "This is my default action...";
+        }
+        // 
+        // GET: /HelloWorld/Welcome/ 
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
         }
     }
 }
---
+++ starSystems/Models/Planets.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace starSystems.Models
+{
+    public class Planets
+    {
+        [Key]
+        public int Id { get; set; }
+        [MaxLength(200)]
+        public string Title { get; set; }
+        [MaxLength(500)]
+        public string Description { get; set; }
+
+        public int StarId { get; set; }
+        [MaxLength(500)]
+        public string StarSystem { get; set; }
+    }
+}
