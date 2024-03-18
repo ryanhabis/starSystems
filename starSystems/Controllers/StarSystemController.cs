9043657ca828e10ef6f0ebec645905d661c09e59
Ryan Habis <ryan.habiss@gmail.com>
2024-03-18 15:36:59 +0000

    Adding controller

---
+++ starSystems/Controllers/StarSystemController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace starSystems.Controllers
+{
+    public class StarSystemController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
