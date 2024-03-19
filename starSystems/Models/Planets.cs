4a24a7d9d60ac5062d9cd0894b37e0a93612b10e
Ryan Habis <ryan.habiss@gmail.com>
2024-03-19 02:56:01 +0000

    added [Required] field in Planets.cs

--- starSystems/Models/Planets.cs
+++ starSystems/Models/Planets.cs
@@ -5,9 +5,13 @@ namespace starSystems.Models
     public class Planets
     {
         public int Id { get; set; }
+        [Required]
         public string? Title { get; set; }
+        [Required]
         public string? Description { get; set; }
+        [Required]
         public int StarId { get; set; }
+        [Required]
         public string? StarSystem { get; set; }
     }
 }
