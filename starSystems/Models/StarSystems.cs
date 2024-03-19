a7f201091cad83327325871be2926db295717514
Evan <evanmcdonnell09@gmail.com>
2024-03-19 02:20:26 +0000

    Added the StarSystem file

---
+++ starSystems/Models/StarSystems.cs
@@ -0,0 +1,11 @@
+﻿namespace starSystems.Models
+{
+    public class StarSystems
+    {
+        public int StarId { get; set; }
+
+        public string StarName { get; set; }
+
+        public string Description { get; set; }
+    }
+}
