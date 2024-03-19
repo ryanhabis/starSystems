dc9d5d8ec72927fe7c1214f390ffb687b81f5e44
Ryan Habis <ryan.habiss@gmail.com>
2024-03-19 02:32:55 +0000

    Tried to test

---
+++ starSystems/UnitTests/UnitTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+using static starSystems.Controllers.PlanetsController;
+
+namespace starSystems.UnitTests
+{
+    public class UnitTest
+    {
+        // Test CRUD
+
+        //Test for Create
+        [Fact]
+        public void Create() 
+        {
+            
+        }
+
+        //Test for read
+        [Fact]
+        public void read()
+        {
+
+        }
+
+        //Test for update
+        [Fact]
+        public void update()
+        {
+
+        }
+        //Test for delete
+        [Fact]
+        public void delete()
+        {
+
+        }
+    }
+}
